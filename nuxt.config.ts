f970a68bf98c3ef253105b24f9cc77c817514697
Sarvarchik2 <sarvarikvarvarik@gmail.com>
2025-09-02 20:59:20 +0500

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://147.93.155.49/api/v1', // для $api
-      apiBaseRoot: process.env.NUXT_PUBLIC_API_BASE_ROOT || 'http://147.93.155.49/api/v1' // для $auth
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://homey.uz/api/v1', // для $api
+      apiBaseRoot: process.env.NUXT_PUBLIC_API_BASE_ROOT || 'https://homey.uz/api/v1' // для $auth
     }
   },
   typescript: { strict: true },
